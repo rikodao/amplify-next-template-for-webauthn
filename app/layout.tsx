02b7eeafb8a4224608eb18e9028ce683b9a0fc28
rikodao <rikodao@gmail.com>
2025-07-11 16:37:16 +0900

    added authenticator

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
-
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+        
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>      
+        <Authenticator>
+          {children}
+        </Authenticator>
+      </body>
     </html>
   );
 }
